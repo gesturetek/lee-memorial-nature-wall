8385e2ba16b4ea829d26dddb3fd00215c3b8e79f
Ryan Miller <ryan@reptoidgames.com>
2017-01-12 11:56:38 -0500

    Environment & Water tests

--- Assets/Ripple/ClickToRipple.cs
+++ Assets/Ripple/ClickToRipple.cs
@@ -3,25 +3,18 @@ using System.Collections;
 
 public class ClickToRipple : MonoBehaviour {
 
-    public Plane plane;
     public Camera cam;
     public ParticleSystem fx;
-
-    private void Start()
-    {
-        plane = new Plane(Vector3.up, Vector3.zero);
-    }
 
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
             Ray r = cam.ScreenPointToRay(Input.mousePosition);
-            float hit;
-            if (plane.Raycast(r, out hit))
+            RaycastHit hit;
+            if (Physics.Raycast(r, out hit))
             {
-                fx.transform.position = r.GetPoint(hit);
+                fx.transform.position = hit.point;
                 fx.Play();
-                //fx.Emit(1);
             }
         }
 	}
