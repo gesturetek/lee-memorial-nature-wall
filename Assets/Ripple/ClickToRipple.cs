5f47cbf523689c871f278980518bc302a6f86582
Ryan Miller <ryan@reptoidgames.com>
2017-01-12 12:43:10 -0500

    Further ripple development

--- Assets/Ripple/ClickToRipple.cs
+++ Assets/Ripple/ClickToRipple.cs
@@ -6,16 +6,20 @@ public class ClickToRipple : MonoBehaviour {
     public Camera cam;
     public ParticleSystem fx;
 
-	void Update () {
-        if (Input.GetMouseButtonDown(0))
+    private void Start()
+    {
+
+        InvokeRepeating("ClickStuff", 0, 0.2f);
+
+    }
+
+	void ClickStuff() {
+        Ray r = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(r, out hit))
         {
-            Ray r = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(r, out hit))
-            {
-                fx.transform.position = hit.point;
-                fx.Play();
-            }
+            fx.transform.position = hit.point;
+            fx.Play();
         }
 	}
 }
